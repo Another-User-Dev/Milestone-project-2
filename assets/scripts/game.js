1ddd2676747d729dc6be1e570bdaf01bca975c53
Another-User-Dev <michael.jobsearch.emails@gmail.com>
2024-01-05 05:09:04 +0000

    Add checkAnswers function

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -1,12 +1,45 @@
 // Wait for the DOM to finish loading before running the game
 // Get the button elements and add event listeners to them
 
+// data for questions
+
+var questions = [
+    {
+    question: 'Which fruit is used to make Cider?',
+    options: ['Apple', 'Orange', 'Grapes', 'Kiwi'],
+    answer: 'c',}, 
+    {
+    question: 'Which fruit is used to make Wine?',
+    options: ['Banana', 'Pineapple', 'Apple', 'Grapes'],
+    answer: 'd',}, 
+    {
+    question: 'Which fruit has a yellow skin?',
+    options: ['Orange', 'Banana', 'Blueberries', 'Grapes'],
+    answer: 'b'}, 
+    {
+    question: 'Which vegetable shares the same name of a capital city?',
+    options: ['Mushroom', 'Cabbage', 'Brussel Sprout', 'Broccoli'],
+    answer: 'c'}, 
+    {
+    question: 'Which vegetable can grow in the dark?',
+    options: ['Brussel Sprout', 'Mushroom', 'Cabbage', 'Tomato'],
+    answer: 'd'},
+]
+
+// declaring variables
+
+let correctScore = 0;
+let wrongScore = 0;
+let questionCount = 0;
+let totalQuestion = (questions.length);
+let buttonLetter ;
+
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons) {
         button.addEventListener("click", function() {
-            let buttonLetter = this.getAttribute("value");
+            buttonLetter = this.getAttribute("value");
             if (buttonLetter === "a") {
                 checkAnswers();
             }
@@ -36,7 +69,15 @@ document.addEventListener("DOMContentLoaded", function() {
 // check if button = right answer add score
 
 function checkAnswers () {
-    console.log('check answer');
+    if (buttonLetter === questions[questionCount].answer) {
+        correctScore++ ;
+        questionCount++ ;
+        console.log(buttonLetter, correctScore, questionCount); 
+        }
+    else {
+        console.log('wrong answer');
+        questionCount++ ;          
+    }
 }
 
 // addScores function()
@@ -44,22 +85,3 @@ function checkAnswers () {
 // display function () correct/wrong answer with running score - set timer - return to game or end if last question
 
 // endofGame function () notifying end of game, number of questions correctly answered with button to restart quiz
-
-// data for questions
-
-var questions = {
-    question: 'Which fruit is used to make Cider?',
-    options: ['Apple', 'Orange', 'Grapes', 'Kiwi'],
-    answer: 'a',
-    question: 'Which fruit is used to make Wine?',
-    options: ['Banana', 'Pineapple', 'Apple', 'Grapes'],
-    answer: 'd',
-    question: 'Which fruit has a yellow skin?',
-    options: ['Orange', 'Banana', 'Blueberries', 'Grapes'],
-    answer: 'b',
-    question: 'Which vegetable shares the same name of a capital city?',
-    options: ['Mushroom', 'Cabbage', 'Brussel Sprout', 'Broccoli'],
-    answer: 'c',
-    question: 'Which vegetable can grow in the dark?',
-    options: ['Brussel Sprout', 'Mushroom', 'Cabbage', 'Tomato'],
-    answer: 'd'}
